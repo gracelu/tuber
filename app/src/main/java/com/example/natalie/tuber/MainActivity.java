69055caa60385df7940cac40ab71b8fad5cf0603
Grace <gracelu1011@gmail.com>
2015-08-04 23:51:16 -0700

    update uber info

--- app/src/main/java/com/example/natalie/tuber/MainActivity.java
+++ app/src/main/java/com/example/natalie/tuber/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.natalie.tuber;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +21,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getResultsButton();
     }
 
     @Override
@@ -34,5 +45,85 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getResultsButton(){
+        Button button = (Button) findViewById(R.id.button);
+
+        final EditText start_address  = (EditText)findViewById(R.id.editText);
+
+        button.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", start_address.getText().toString());
+                        getUberCost(1, 1, 1, 1);
+                    }
+                });
+    }
+
+    public double getUberCost(double startLatitute, double startLongitude, double endLatitude, double endLongitude){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        String uberData = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are available at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+
+            //https://api.uber.com/v1/estimates/price?server_token=28t86KafW8mM1WjOVyQJ2_rLmVWEU71LVeC5ucjW&start_latitude=37.775818&start_longitude=-122.418028&end_latitude=37.775838&end_longitude=-122.118028
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            /*InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                uberData = null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                uberData = null;
+            }
+            uberData = buffer.toString(); */
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attempting
+            // to parse it.
+            uberData = null;
+        } /*finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }*/
+
+        //Log.v("Uber Data", uberData);
+        Log.v("UberData", "here");
+        return 0;
     }
 }
