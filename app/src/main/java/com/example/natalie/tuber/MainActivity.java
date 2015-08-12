420e0f2bc70de0f37aa84cbbf0e37771f1c82f58
Natalie <colemann@mit.edu>
2015-08-11 21:16:04 -0400

    xml changes

--- app/src/main/java/com/example/natalie/tuber/MainActivity.java
+++ app/src/main/java/com/example/natalie/tuber/MainActivity.java
@@ -1,21 +1,78 @@
 package com.example.natalie.tuber;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new QueryFragment())
-                    .commit();
-        }
+        setContentView(R.layout.activity_geocoding);
+
+        final EditText origin = (EditText) findViewById(R.id.EditText01);
+        final EditText destination = (EditText) findViewById(R.id.EditText02);
+        final double[] originLocation = new double[2];
+        final double[] destinationLocation = new double[2];
+
+        final Button[] addressButton = {(Button) findViewById(R.id.calculateButton)};
+        addressButton[0].setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GeocodingLocation locationAddress = new GeocodingLocation();
+                originLocation[0] = locationAddress.getAddressFromLocation(origin.getText().toString(), getApplicationContext())[0];
+                originLocation[1] = locationAddress.getAddressFromLocation(origin.getText().toString(), getApplicationContext())[1];
+
+                destinationLocation[0] = locationAddress.getAddressFromLocation(destination.getText().toString(), getApplicationContext())[0];
+                destinationLocation[1] = locationAddress.getAddressFromLocation(destination.getText().toString(), getApplicationContext())[1];
+
+                String uberOrigLat = Double.toString(originLocation[0]);
+                String uberOrigLong = Double.toString(originLocation[1]);
+                String uberDestLat = Double.toString(destinationLocation[0]);
+                String uberDestLong = Double.toString(destinationLocation[1]);
+
+                String taxiOrig = uberOrigLat + "," + uberOrigLong;
+                String taxiDest = uberDestLat + "," + uberDestLong;
+
+                Log.v("Lat", uberOrigLat);
+                Log.v("Long", uberOrigLong);
+                Log.v("DestLat", uberDestLat);
+                Log.v("DestLong", uberDestLong);
+
+                Log.v("taxiOrig", taxiOrig);
+                Log.v("taxiDest", taxiDest);
+
+                ArrayAdapter<String> mQueryAdapterTaxi =
+                        new ArrayAdapter<String>(
+                                MainActivity.this, // The current context (this activity)
+                                R.layout.taxi_list_item, // The name of the layout ID.
+                                R.id.list_item_taxi_textview, // The ID of the textview to populate.
+                                new ArrayList<String>());
+
+
+                String api_key = getString(R.string.taxifarefinder_api_key);
+                FetchTaxiPriceTask taxiTask = new FetchTaxiPriceTask(MainActivity.this, api_key, mQueryAdapterTaxi);
+                AsyncTask<String, Void, String[]> taxresults = taxiTask.execute(taxiOrig, taxiDest);
+                Log.v("TaxiFare", "0" );
+
+
+                setContentView(R.layout.fragment_query);
+
+
+
+            }
+        });
     }
 
     @Override
@@ -40,11 +97,10 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-//
+
 //    public void getResultsButton2() {
 //        Button button = (Button) findViewById(R.id.button);
-//        final EditText origin = (EditText) findViewById(R.id.editText);
-//        final EditText destination = (EditText) findViewById(R.id.editText2);
+//
 //        final String api_key = getString(R.string.taxifarefinder_api_key);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View view) {
