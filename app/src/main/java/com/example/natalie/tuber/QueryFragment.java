0fe8c1410d9b1ded21dddb84e6aac4d7bc87dda5
Grace <gracelu1011@gmail.com>
2015-08-15 11:47:00 -0700

    update refresh

--- app/src/main/java/com/example/natalie/tuber/QueryFragment.java
+++ app/src/main/java/com/example/natalie/tuber/QueryFragment.java
@@ -1,7 +1,6 @@
 package com.example.natalie.tuber;
 
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
@@ -12,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.natalie.tuber.data.PriceContract;
@@ -38,11 +36,19 @@ public class QueryFragment extends Fragment implements LoaderManager.LoaderCallb
         // Required empty public constructor
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
